2a38268a2d90c7af6c9a4e9a2e2c41d5d8b0a584
Nicolfo <nicolfo@hotmail.it>
2022-03-17 01:31:50 +0100

    Conversione definizione oggetto in classe

--- LAB1-Nicolò/lab1.js
+++ LAB1-Nicolò/lab1.js
@@ -8,10 +8,10 @@ function Film (id,title,favorites=false,date,rating){
     this.favorites=favorites;
 }
 
-let FilmLibrary={
-    filmati : [],
-    addNewFilm : (film)=>{FilmLibrary.filmati.push(film)},
-    sortByDate : () => {
+function FilmLibrary(){
+    this.filmati = [];
+    this.addNewFilm =(film)=>{this.filmati.push(film)};
+    this.sortByDate = () => {
         FilmLibrary.filmati.sort((x,y)=>{
             if(x.date==undefined)
                 return 1;
@@ -19,56 +19,56 @@ let FilmLibrary={
                 return -1;
             return x.date.diff(y.date);
         });
-    },
-    printer: (type=true)=>{
+    };
+    this.printer=(type=true)=>{
         if(type)
             console.log("***** List of films *****");
-        for(const a of FilmLibrary.filmati){
+        for(const a of this.filmati){
            if(a.date!=undefined)
             console.log(`Id: ${a.id}, Title: ${a.title}, Favorite: ${a.favorites}, Whatch Date: ${a.date.format("DD-MM-YY")}, Rating: ${a.rating||"<not assigned>"}`);
             else 
             console.log(`Id: ${a.id}, Title: ${a.title}, Favorite: ${a.favorites}, Whatch Date: <not defined>, Rating: ${a.rating||"<not assigned>"}`);
         }
-    },
-    deleteFilm : (f_id)=>{
+    };
+    this.deleteFilm =(f_id)=>{
         let pos=0;
         
-        for(let f of FilmLibrary.filmati){
+        for(let f of this.filmati){
             if(f_id==f.id)
                 break;
             pos++;
         }
-        FilmLibrary.filmati.splice(pos,1);
-    },
-    resetWatchedFilms : () => {
-        for(let f of FilmLibrary.filmati){
+        this.filmati.splice(pos,1);
+    };
+    this.resetWatchedFilms = () => {
+        for(let f of this.filmati){
             f.date=undefined;
         }
-    },
-    getRated : ()=>{
-        let copy=[...FilmLibrary.filmati];
+    };
+    this.getRated = ()=>{
+        let copy=[...this.filmati];
         console.log("***** Films filtered, only the rated ones *****");
-        for(let i=0; i<FilmLibrary.filmati.length;i++){
-            if(FilmLibrary.filmati[i].rating===undefined){
-                FilmLibrary.filmati.splice(i,1);
+        for(let i=0; i<this.filmati.length;i++){
+            if(this.filmati[i].rating===undefined){
+                this.filmati.splice(i,1);
                 i--;
             }
         }
-        FilmLibrary.filmati.sort((x,y)=>(x.rating-y.rating));
-        FilmLibrary.printer(false);
-        let copy2=[...FilmLibrary.filmati];                         //si poteva evitare lavorando su copy direttamente ma non
-        FilmLibrary.filmati=[...copy];                              //volevo rifare il metodo di stampa
+        this.filmati.sort((x,y)=>(x.rating-y.rating));
+        this.printer(false);
+        let copy2=[...this.filmati];                         //si poteva evitare lavorando su copy direttamente ma non
+        this.filmati=[...copy];                              //volevo rifare il metodo di stampa
         return copy2;
     }
 
 }
-
-FilmLibrary.addNewFilm(new Film(1,"Pulp Fiction",true,new dayjs('10 March 2022'),5));
-FilmLibrary.addNewFilm(new Film(2,"21 Gram",true,new dayjs('17 March 2022'),4));
-FilmLibrary.addNewFilm(new Film(3,"Star Wars",false));
-FilmLibrary.addNewFilm(new Film(4,"Matrix",false));
-FilmLibrary.addNewFilm(new Film(5,"Shrek",false,new dayjs('21 March 2022'),3));
+let fl = new FilmLibrary();
+fl.addNewFilm(new Film(1,"Pulp Fiction",true,new dayjs('10 March 2022'),5));
+fl.addNewFilm(new Film(2,"21 Gram",true,new dayjs('17 March 2022'),4));
+fl.addNewFilm(new Film(3,"Star Wars",false));
+fl.addNewFilm(new Film(4,"Matrix",false));
+fl.addNewFilm(new Film(5,"Shrek",false,new dayjs('21 March 2022'),3));
 
 //FilmLibrary.printer();
-FilmLibrary.printer();
-FilmLibrary.getRated();
+fl.printer();
+fl.getRated();
